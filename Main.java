04f3514d9924a706a168f6725b3acdb5bb961788
Kaylen Miller <kayjmiller05@gmail.com>
2025-06-09 21:16:43 -0400

    Main Method

---
+++ Main.java
@@ -0,0 +1,15 @@
+package driverPackage;
+
+import helperPackage.Novel;
+import helperPackage.Textbook;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Textbook textBookObject = new Textbook("Algebra for Cryptologists", "Alko R. Meijer", "Springer", 2016, 69.99); 
+        Novel novelObject = new Novel("War and Peace", "Leo Tolstory", "The Russian Messenger", 1869, 225);
+    }
+}
+    
+        
